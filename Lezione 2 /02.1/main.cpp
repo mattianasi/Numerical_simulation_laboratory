1bf4ea9b505b8f4b59d023db020fa54ea18827d6
Mattia Nasi <mattia@MacBook-Air-di-Mattia.local>
2025-05-31 12:26:27 +0200

    Only final check needed

--- Lezione 2 /02.1/main.cpp
+++ Lezione 2 /02.1/main.cpp
@@ -6,114 +6,132 @@
 
 using namespace std;
 
+// Function to calculate statistical error of the mean
 double error(vector<double>, vector<double>, int);
+
+// Functions to integrate
 double function(double);
 double function1(double);
 
 int main(){
 
-Random rand;
+    Random rand;  // Random number generator object
 
-int seed[4];
-int p1, p2;
-ifstream Primes("Primes");
-if (Primes.is_open()){
-    Primes >> p1 >> p2 ;
-} else cerr << "PROBLEM: Unable to open Primes" << endl;
-Primes.close();
+    int seed[4];
+    int p1, p2;
+    // Read prime numbers needed to initialize RNG
+    ifstream Primes("Primes");
+    if (Primes.is_open()){
+        Primes >> p1 >> p2 ;
+    } else cerr << "PROBLEM: Unable to open Primes" << endl;
+    Primes.close();
 
-ifstream input("seed.in");
-string property;
-if (input.is_open()){
-    while ( !input.eof() ){
-        input >> property;
-        if( property == "RANDOMSEED" ){
-            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
-            rand.SetRandom(seed,p1,p2);
+    // Read seed values from file and initialize RNG
+    ifstream input("seed.in");
+    string property;
+    if (input.is_open()){
+        while ( !input.eof() ){
+            input >> property;
+            if( property == "RANDOMSEED" ){
+                input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+                rand.SetRandom(seed,p1,p2);
+            }
         }
+        input.close();
+    } else cerr << "PROBLEM: Unable to open seed.in" << endl;
+
+    int M = 100000; // Total number of random samples
+    int N = 100;    // Number of blocks for blocking method
+    int L = M/N;    // Number of samples per block
+
+    vector<double> sum(N);   // Block averages
+    vector<double> sum2(N);  // Square of block averages for variance calculation
+    vector<double> err(N);   // Errors from blocking method
+    ofstream out1,out2;
+
+    // First integration: using uniform random numbers (rand.Rannyu())
+    for(int i=0; i<N; i++){
+        double f = 0;
+        for(int j=0; j<L; j++){
+            double x = rand.Rannyu();   // Uniform random number in [0,1)
+            f += function(x);           // Evaluate integrand at x
+        }
+        sum[i] = f/L;                   // Average value of function in block i
+        sum2[i] = sum[i]*sum[i];       // Store square for error estimation
     }
-    input.close();
-} else cerr << "PROBLEM: Unable to open seed.in" << endl;
 
-int M = 100000;
-int N = 100;
-int L = M/N;
+    // Open output file for first integral
+    out1.open("data.dat");
 
-vector<double> sum(N);
-vector<double> sum2(N);
-vector<double> err(N);
-ofstream out1,out2;
+    vector<double> averages(N, 0.0);   // Progressive averages over blocks
+    vector<double> averages2(N, 0.0);  // Progressive averages of squared values
 
-for(int i=0; i<N; i++){
-    double f = 0;
-    for(int j=0; j<L; j++){
-        double x = rand.Rannyu();
-        f += function(x);
+    // Calculate progressive averages and errors using blocking method
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            averages[i] += sum[j];
+            averages2[i] += sum2[j];
+        }
+        averages[i] /= (i + 1);
+        averages2[i] /= (i + 1);
+        err[i] = error(averages, averages2, i);
+        out1 << averages[i] << "," << err[i] << endl;  // Write average and error
     }
-    sum[i] = f/L;
-    sum2[i] = sum[i]*sum[i];
+    out1.close();
+
+    // Second integration: using a different distribution (rand.Distr())
+    vector<double> sum_(N);
+    vector<double> sum2_(N);
+    vector<double> err_(N);
+
+    for(int i=0; i<N; i++){
+        double f = 0;
+        for(int j=0; j<L; j++){
+            double x = rand.Distr();      // Random number from custom distribution
+            f += function1(x);            // Evaluate integrand adapted for this distribution
+        }
+        sum_[i] = f/L;
+        sum2_[i] = sum_[i]*sum_[i];
+    }
+
+    // Output file for second integral
+    out2.open("data1.dat");
+
+    vector<double> averages_(N, 0.0);
+    vector<double> averages2_(N, 0.0);
+
+    // Calculate progressive averages and errors for second integral
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            averages_[i] += sum_[j];
+            averages2_[i] += sum2_[j];
+        }
+        averages_[i] /= (i + 1);
+        averages2_[i] /= (i + 1);
+        err_[i] = error(averages_, averages2_, i);
+        out2 << averages_[i] << "," << err_[i] << endl;
+    }
+    out2.close();
+
+    return 0;
 }
 
-out1.open("data.dat");
-vector<double> averages(N, 0.0);
-vector<double> averages2(N, 0.0);
-
-for (int i = 0; i < N; i++) {
-    for (int j = 0; j < i + 1; j++) {
-        averages[i] += sum[j];
-        averages2[i] += sum2[j];
-    }
-    averages[i] /= (i + 1);
-    averages2[i] /= (i + 1);
-    err[i] = error(averages, averages2, i);
-    out1 << averages[i] << "," << err[i] << endl;
-}
-out1.close();
-
-vector<double> sum_(N);
-vector<double> sum2_(N);
-vector<double> err_(N);
-
-for(int i=0; i<N; i++){
-    double f = 0;
-    for(int j=0; j<L; j++){
-        double x = rand.Distr();
-        f += function1(x);
-    }
-    sum_[i] = f/L;
-    sum2_[i] = sum_[i]*sum_[i];
-}
-
-out2.open("data1.dat");
-vector<double> averages_(N, 0.0);
-vector<double> averages2_(N, 0.0);
-
-for (int i = 0; i < N; i++) {
-    for (int j = 0; j < i + 1; j++) {
-        averages_[i] += sum_[j];
-        averages2_[i] += sum2_[j];
-    }
-    averages_[i] /= (i + 1);
-    averages2_[i] /= (i + 1);
-    err_[i] = error(averages_, averages2_, i);
-    out2 << averages_[i] << "," << err_[i] << endl;
-}
-out2.close();
-
-return 0;
-}
-
+// Function to compute statistical error for progressive averages
 double error(vector<double> AV, vector<double> AV2, int n){
-        if(n == 0) {
-            return 0;}
-        else{
-            return sqrt((AV2[n]-pow(AV[n],2))/n);}
-};
+    if(n == 0) {
+        return 0;  // No error for first point
+    } else {
+        // Standard error of the mean formula
+        return sqrt((AV2[n]-pow(AV[n],2))/n);
+    }
+}
 
+// Function to be integrated in first method
 double function(double x){
     return M_PI/2*cos(M_PI*x/2);
 }
 
+// Function to be integrated in second method, designed to work with importance sampling, calculated from Taylor expansion 
 double function1(double x){
     return M_PI/4*(cos(M_PI*x/2))/(1-x);
 }
--- Lezione 2 /02.2/main.cpp
+++ Lezione 2 /02.2/main.cpp
@@ -9,125 +9,127 @@ using namespace std;
 
 double error(vector<double>, vector<double>, int);
 
+int main() {
+    Random rand;
+    
+    int N = 100;         // Number of blocks
+    int M = 100000;      // Total number of simulations
+    int L = M / N;       // Simulations per block
+    double steps = 100;  // Number of steps in the random walk
 
-int main(){
+    int seed[4];
+    int p1, p2;
 
-Random rand;
-int N = 100;
-int M = 100000;
-int L = M/N;
-double x;
-double step = 100;
+    // Initialize RNG from external files
+    ifstream Primes("Primes");
+    if (Primes.is_open()) {
+        Primes >> p1 >> p2;
+    } else cerr << "PROBLEM: Unable to open Primes" << endl;
+    Primes.close();
 
-int seed[4];
-int p1, p2;
-ifstream Primes("Primes");
-if (Primes.is_open()){
-    Primes >> p1 >> p2 ;
-} else cerr << "PROBLEM: Unable to open Primes" << endl;
-Primes.close();
-
-ifstream input("seed.in");
-string property;
-if (input.is_open()){
-    while ( !input.eof() ){
-        input >> property;
-        if( property == "RANDOMSEED" ){
-            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
-            rand.SetRandom(seed,p1,p2);
-        }
-    }
-    input.close();
-} else cerr << "PROBLEM: Unable to open seed.in" << endl;
-
-vector<double> sum(N);
-vector<double> sum2(N);
-vector<double> err(N);
-ofstream out1;
-double interval[6] = {1.0/6, 2.0/6, 3.0/6, 4.0/6, 5.0/6, 1.0};
-
-for(int i=0; i<N; i++){
-    vector<double> v(N, 0.0);
-    for(int j=0; j<L; j++){
-        vector<double> pos(6, 0.0);
-        for(int k = 0; k<step; k++){
-            x = rand.Rannyu();
-            for(int m=0; m<6; m++){
-                if(x<=interval[m]){
-                pos[m]+=1;
-                break;
-                }
+    ifstream input("seed.in");
+    string property;
+    if (input.is_open()) {
+        while (!input.eof()) {
+            input >> property;
+            if (property == "RANDOMSEED") {
+                input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+                rand.SetRandom(seed, p1, p2);
             }
-            v[k] += pow((pos[0]-pos[1]),2)+pow((pos[2]-pos[3]),2)+pow((pos[4]-pos[5]),2);
+        }
+        input.close();
+    } else cerr << "PROBLEM: Unable to open seed.in" << endl;
+
+    // 1. DISCRETE RANDOM WALK
+    vector<double> sum_discrete(N, 0.0);
+    vector<double> sum2_discrete(N, 0.0);
+    vector<double> err_discrete(N, 0.0);
+    double interval[6] = {1.0/6, 2.0/6, 3.0/6, 4.0/6, 5.0/6, 1.0}; // 6 directions
+
+    for (int i = 0; i < N; i++) {
+        vector<double> distance(steps, 0.0);
+        for (int j = 0; j < L; j++) {
+            vector<double> pos(6, 0.0);  // Discrete directions counter
+
+            for (int k = 0; k < steps; k++) {
+                double x = rand.Rannyu(); // Random number to determine direction
+                for (int m = 0; m < 6; m++) {
+                    if (x <= interval[m]) {
+                        pos[m] += 1;
+                        break;
+                    }
+                }
+                // Distance squared from origin after k+1 steps
+                distance[k] += pow(pos[0]-pos[1], 2) + pow(pos[2]-pos[3], 2) + pow(pos[4]-pos[5], 2);
+            }
+        }
+        for (int k = 0; k < steps; k++) {
+            distance[k] /= L;             // Average over L simulations
+            distance[k] = sqrt(distance[k]); // Root mean square distance
+            sum_discrete[k] += distance[k];
+            sum2_discrete[k] += distance[k] * distance[k];
         }
     }
-    for(int k = 0; k<step; k++){
-        v[k]/=L;
-        v[k] = sqrt(v[k]);
+
+    ofstream out1("data.dat");
+    for (int i = 0; i < steps; i++) {
+        sum_discrete[i] /= N;
+        sum2_discrete[i] /= N;
+        err_discrete[i] = sqrt((sum2_discrete[i] - pow(sum_discrete[i], 2)) / N);
+        out1 << sum_discrete[i] << "," << err_discrete[i] << endl;
     }
-    for(int k=0; k<step; k++){
-        sum[k] += v[k];
-        sum2[k] += v[k]*v[k];
-    }
-}
+    out1.close();
 
-out1.open("data.dat");
+    // 2. CONTINUOUS RANDOM WALK
+    vector<double> sum_cont(N, 0.0);
+    vector<double> sum2_cont(N, 0.0);
+    vector<double> err_cont(N, 0.0);
 
+    for (int i = 0; i < N; i++) {
+        vector<double> distance(steps, 0.0);
 
-for (int i = 0; i < step; i++) { //anche sui blocchi? in caso modificare
-    sum[i]/=(step);
-    sum2[i]/=(step);
-    err[i] = sqrt((sum2[i]-pow(sum[i],2))/step); 
-    out1 << sum[i] << "," << err[i] << endl;
-}
-out1.close();
+        for (int j = 0; j < L; j++) {
+            vector<double> r(3, 0.0); // x, y, z
 
-vector<double> sum_cont(N);
-vector<double> sum_cont2(N);
-vector<double> error_cont(N);
-ofstream out2;
+            for (int k = 0; k < steps; k++) {
+                double a = rand.Rannyu();
+                double phi = rand.Rannyu(0, 2*M_PI);
+                double theta = acos(1 - 2*a); // Uniform over sphere
 
-for(int i=0; i<N; i++){
-    vector<double> w(N, 0.0);
-    for(int j=0; j<L; j++){
-        vector<double> r(3,0.0);
-        for(int k = 0; k<step; k++){
-            double a = rand.Rannyu();
-            double phi = rand.Rannyu(0,2*M_PI);
-            double theta = acos(1-2*a);
-            r[0] += sin(theta)*cos(phi);
-            r[1] += sin(theta)*sin(phi);
-            r[2] += cos(theta);
-            w[k] += r[0]*r[0]+r[1]*r[1]+r[2]*r[2];
+                // Step in spherical coordinates
+                r[0] += sin(theta) * cos(phi);
+                r[1] += sin(theta) * sin(phi);
+                r[2] += cos(theta);
+
+                distance[k] += r[0]*r[0] + r[1]*r[1] + r[2]*r[2]; // r²
+            }
+        }
+
+        for (int k = 0; k < steps; k++) {
+            distance[k] /= L;
+            distance[k] = sqrt(distance[k]);
+            sum_cont[k] += distance[k];
+            sum2_cont[k] += distance[k] * distance[k];
         }
     }
-    for(int k = 0; k<step; k++){
-        w[k]/=L;
-        w[k] = sqrt(w[k]);
+
+    ofstream out2("datacont.dat");
+    for (int i = 0; i < steps; i++) {
+        sum_cont[i] /= N;
+        sum2_cont[i] /= N;
+        err_cont[i] = sqrt((sum2_cont[i] - pow(sum_cont[i], 2)) / N);
+        out2 << sum_cont[i] << "," << err_cont[i] << endl;
     }
-    for(int k=0; k<step; k++){
-        sum_cont[k] += w[k];
-        sum_cont2[k] += w[k]*w[k];
+    out2.close();
+
+    return 0;
+}
+
+// Standard statistical error calculation
+double error(vector<double> AV, vector<double> AV2, int n) {
+    if (n == 0) {
+        return 0;
+    } else {
+        return sqrt((AV2[n] - pow(AV[n], 2)) / n);
     }
 }
-
-out2.open("datacont.dat");
-
-
-for (int i = 0; i < N; i++) {
-    sum_cont[i]/=(step);
-    sum_cont2[i]/=(step);
-    error_cont[i] = sqrt((sum_cont2[i]-pow(sum_cont[i],2))/step);
-    out2 << sum_cont[i] << "," << error_cont[i] << endl;
-}
-out2.close();
-
-return 0;
-}
-
-double error(vector<double> AV, vector<double> AV2, int n){
-        if(n == 0) {
-            return 0;}
-        else{
-            return sqrt((AV2[n]-pow(AV[n],2))/n);}
-};
