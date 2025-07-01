0d05677b79e0369f3ae6bbffa0360691ed1ceeae
Mattia Nasi <mattia@MacBook-Air-di-Mattia.local>
2025-07-01 11:07:34 +0200

    Only final check

--- Lezione 10/.ipynb_checkpoints/main-checkpoint.cpp
+++ Lezione 10/.ipynb_checkpoints/main-checkpoint.cpp
@@ -23,9 +23,9 @@ int main(int argc, char* argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     const int ncap = 110;
-    const int npop = 200;
-    const int ngen = 10000;
-    const int Nmigr = 100001; // Migration frequency
+    const int npop = 100;
+    const int ngen = 100000;
+    const int Nmigr = 500; // Migration frequency
 
     mat dist_matrix(ncap, ncap);
     load_cities(dist_matrix);
